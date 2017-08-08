0b57b6bd9c45a9a4afaa58e211ccaed1f87e9f53
Leomar Bucud <leomarbucud@gmail.com>
2017-08-08 17:21:40 +0400

    Change G background color on default avatar, remove login page on link

--- assets/js/letter-avatar.js
+++ assets/js/letter-avatar.js
@@ -6,7 +6,7 @@
 * based on https://gist.github.com/leecrossley/6027780
 */
 (function(w, d){
-  
+
 
   function LetterAvatar (name, size) {
 
@@ -14,7 +14,7 @@
     size  = size || 60;
 
     var colours = [
-      "#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#34495e", "#16a085", "#27ae60", "#2980b9", "#8e44ad", "#2c3e50",
+      "#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#34495e", "#16a085", "#FF3B30", "#2980b9", "#8e44ad", "#2c3e50",
       "#f1c40f", "#e67e22", "#e74c3c", "#ecf0f1", "#95a5a6", "#f39c12", "#d35400", "#c0392b", "#bdc3c7", "#7f8c8d"
     ],
 
