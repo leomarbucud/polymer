aa80d9ef3a73cce9577cc88a909ff1bf2ca7c4b1
Leomar Bucud <leomarbucud@gmail.com>
2017-09-26 17:48:53 +0400

    Update service worker, cached then network

--- service-worker-bak.js
+++ service-worker-bak.js
@@ -21,34 +21,34 @@ var cacheName = 'v1.1.1';
 var cacheFiles = [
 	'./',
 	'./index.html',
-  "./elements/thegrid/grid-app.html",
-  "./elements/thegrid/grid-icons.html",
-  "./elements/thegrid/grid-styles.html",
-  "./elements/thegrid/components/carousel.html",
-  "./elements/thegrid/components/datepicker.html",
-  "./elements/thegrid/components/divider-text.html",
-  "./elements/thegrid/components/drawer.html",
-  "./elements/thegrid/components/grid-dropdown.html",
-  "./elements/thegrid/components/grid-introduction.html",
-  "./elements/thegrid/components/grid-map.html",
-  "./elements/thegrid/components/login-components.html",
-  "./elements/thegrid/components/mobile-header.html",
-  "./elements/thegrid/components/ratings.html",
+	"./elements/thegrid/grid-app.html",
+	"./elements/thegrid/grid-icons.html",
+	"./elements/thegrid/grid-styles.html",
+	"./elements/thegrid/components/carousel.html",
+	"./elements/thegrid/components/datepicker.html",
+	"./elements/thegrid/components/divider-text.html",
+	"./elements/thegrid/components/drawer.html",
+	"./elements/thegrid/components/grid-dropdown.html",
+	"./elements/thegrid/components/grid-introduction.html",
+	"./elements/thegrid/components/grid-map.html",
+	"./elements/thegrid/components/login-components.html",
+	"./elements/thegrid/components/mobile-header.html",
+	"./elements/thegrid/components/ratings.html",
 	"./elements/thegrid/components/social-button.html",
-  "./elements/thegrid/pages/grid-404.html",
-  "./elements/thegrid/pages/grid-filter.html",
-  "./elements/thegrid/pages/grid-forgot.html",
-  "./elements/thegrid/pages/grid-help-feedback.html",
-  "./elements/thegrid/pages/grid-help-inbox.html",
-  "./elements/thegrid/pages/grid-login.html",
-  "./elements/thegrid/pages/grid-posts.html",
-  "./elements/thegrid/pages/grid-profile.html",
-  "./elements/thegrid/pages/grid-register.html",
-  "./elements/thegrid/pages/grid-reset-password.html",
-  "./elements/thegrid/pages/grid-search.html",
-  "./elements/thegrid/pages/grid-settings.html",
-  "./elements/thegrid/pages/grid-user-post.html",
-  "./elements/thegrid/pages/grid-user-profile.html",
+	"./elements/thegrid/pages/grid-404.html",
+	"./elements/thegrid/pages/grid-filter.html",
+	"./elements/thegrid/pages/grid-forgot.html",
+	"./elements/thegrid/pages/grid-help-feedback.html",
+	"./elements/thegrid/pages/grid-help-inbox.html",
+	"./elements/thegrid/pages/grid-login.html",
+	"./elements/thegrid/pages/grid-posts.html",
+	"./elements/thegrid/pages/grid-profile.html",
+	"./elements/thegrid/pages/grid-register.html",
+	"./elements/thegrid/pages/grid-reset-password.html",
+	"./elements/thegrid/pages/grid-search.html",
+	"./elements/thegrid/pages/grid-settings.html",
+	"./elements/thegrid/pages/grid-user-post.html",
+	"./elements/thegrid/pages/grid-user-profile.html",
 	"./elements/thegrid/pages/bids/grid-bid.html",
 	"./elements/thegrid/pages/help/grid-feedback.html",
 	"./elements/thegrid/pages/help/grid-getting-started.html",
@@ -79,44 +79,44 @@ var cacheFiles = [
 	"./elements/thegrid/pages/settings/grid-settings-username.html",
 	"./elements/thegrid/pages/settings/grid-settings-verification.html",
 	"./elements/thegrid/pages/transactions/grid-transaction.html",
-  "./behaviors/map-behavior.html",
-  "./behaviors/form-slider-behavior.html",
-  "./behaviors/navigation-behavior.html",
-  // "./redux/grid-redux-store.html",
-  "./node_modules/redux/dist/redux.js",
-  "./node_modules/axios/dist/axios.js",
-  "./assets/js/grid.js",
-  "./assets/js/markerclusterer.js",
-  "./assets/js/letter-avatar.js",
+	"./behaviors/map-behavior.html",
+	"./behaviors/form-slider-behavior.html",
+	"./behaviors/navigation-behavior.html",
+	// "./redux/grid-redux-store.html",
+	"./node_modules/redux/dist/redux.js",
+	"./node_modules/axios/dist/axios.js",
+	"./assets/js/grid.js",
+	"./assets/js/markerclusterer.js",
+	"./assets/js/letter-avatar.js",
 	'https://fonts.googleapis.com/css?family=Roboto'
 ]
 
 
-self.addEventListener('install', function(e) {
-    console.log('[ServiceWorker] Installed');
+self.addEventListener('install', function (e) {
+	console.log('[ServiceWorker] Installed');
 
-    // e.waitUntil Delays the event until the Promise is resolved
-    e.waitUntil(
+	// e.waitUntil Delays the event until the Promise is resolved
+	e.waitUntil(
 
-    	// Open the cache
-	    caches.open(cacheName).then(function(cache) {
+		// Open the cache
+		caches.open(cacheName).then(function (cache) {
 
-	    	// Add all the default files to the cache
+			// Add all the default files to the cache
 			console.log('[ServiceWorker] Caching cacheFiles');
 			return cache.addAll(cacheFiles);
-	    })
+		})
 	); // end e.waitUntil
 });
 
 
-self.addEventListener('activate', function(e) {
-    console.log('[ServiceWorker] Activated');
+self.addEventListener('activate', function (e) {
+	console.log('[ServiceWorker] Activated');
 
-    e.waitUntil(
+	e.waitUntil(
 
-    	// Get all the cache keys (cacheName)
-		caches.keys().then(function(cacheNames) {
-			return Promise.all(cacheNames.map(function(thisCacheName) {
+		// Get all the cache keys (cacheName)
+		caches.keys().then(function (cacheNames) {
+			return Promise.all(cacheNames.map(function (thisCacheName) {
 
 				// If a cached item is saved under a previous cacheName
 				if (thisCacheName !== cacheName) {
@@ -132,89 +132,161 @@ self.addEventListener('activate', function(e) {
 });
 
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
 	// console.log('[ServiceWorker] Fetch', e.request.url);
 
-	if (e.request.url.startsWith(self.location.origin)) {
-    e.respondWith(
-      caches.match(e.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
+	var requestURL = new URL(e.request.url);
 
-        return caches.open(cacheName).then(cache => {
-          return fetch(e.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(e.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
-  // var requestURL = new URL(e.request.url);
-/*
-  if(requestURL.hostname != 'api.thegrid.com') {
-    // console.log('Request is to grid api');
-  } else if(/\.googleapis\.com$/.test(requestURL.hostname) ||
-            /\.gstatic\.com$/.test(requestURL.hostname)) {
-    // console.log('Request is to google api');
-  } else {
-    console.log('[ServiceWorker] Fetch', e.request.url);
+	// Handle requests to a particular host specifically
+	if (requestURL.hostname == 'api.thegrid.com') {
+		if (e.request.method == 'GET') {
 
-  	// e.respondWidth Responds to the fetch event
-  	e.respondWith(
+			if (e.request.headers.get('Cached') == 'yes') {
+				e.respondWith(
+					caches.match(e.request)
+				);
 
-  		// Check in cache for the request being made
-  		caches.match(e.request)
+				return;
+			}
 
-
-  			.then(function(response) {
-
-  				// If the request is in the cache
-  				if ( response ) {
-  					console.log("[ServiceWorker] Found in Cache", e.request.url, response);
-  					// Return the cached version
-  					return response;
-  				}
-
-  				// If the request is NOT in the cache, fetch and cache
-
-  				var requestClone = e.request.clone();
-  				fetch(requestClone)
-  					.then(function(response) {
-
-  						if ( !response ) {
-  							console.log("[ServiceWorker] No response from fetch ")
-  							return response;
-  						}
-
-  						var responseClone = response.clone();
-
-  						//  Open the cache
-  						return caches.open(cacheName).then(function(cache) {
-
-  							// Put the fetched response in the cache
-  							cache.put(e.request, responseClone);
-  							// console.log('aaaaaaaaa', e.request, responseClone);
-  							console.log('[ServiceWorker] New Data Cached', e.request.url);
-
-								console.log('response', response);
-  							// Return the response
-  							return response;
-
-              }); // end caches.open
-
-  					})
-  					.catch(function(err) {
-  						console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
-  					});
-
-
-  			}) // end caches.match(e.request)
-  	); // end e.respondWith
-
+			e.respondWith(
+				caches.open(cacheName).then(function(cache) {
+					return fetch(e.request).then(function(response) {
+						cache.put(e.request, response.clone());
+						console.log('[ServiceWorker] API Data Cached', e.request.url);
+						return response;
+					});
+				})
+				// caches.open(cacheName).then(function(cache) {
+				// 	return cache.match(e.request).then(function (response) {
+						
+				// 		fetch(e.request).then(function(response) {
+				// 			cache.put(e.request, response.clone());
+				// 			return response;
+				// 		});
+						
+				// 		return response || fetch(e.request).then(function(response) {
+				// 			cache.put(e.request, response.clone());
+				// 			return response;
+				// 		});
+				// 	});
+				// })
+				// caches.match(e.request).then(cachedResponse => {
+				// 	if (cachedResponse) {
+				// 		return cachedResponse;
+				// 	}
+	
+				// 	return caches.open(cacheName).then(cache => {
+				// 		return fetch(e.request).then(response => {
+				// 			// Put a copy of the response in the runtime cache.
+				// 			return cache.put(e.request, response.clone()).then(() => {
+				// 				return response;
+				// 			});
+				// 		});
+				// 	});
+				// })
+				// caches.open(cacheName).then(function(cache) {
+				// 	return fetch(e.request).then(function(response) {
+				// 		cache.put(e.request, response.clone());
+				// 		console.log('[ServiceWorker] API Data Cached', e.request.url);
+				// 		return response;
+				// 	});
+				// })
+			);
+			return;
+		}
+		return;
 	}
-*/
+	// Routing for local URLs
+	if (requestURL.origin == location.origin) {
+
+		e.respondWith(
+			caches.match(e.request).then(cachedResponse => {
+				if (cachedResponse) {
+					return cachedResponse;
+				}
+
+				return caches.open(cacheName).then(cache => {
+					return fetch(e.request).then(response => {
+						// Put a copy of the response in the runtime cache.
+						return cache.put(e.request, response.clone()).then(() => {
+							return response;
+						});
+					});
+				});
+			})
+		);
+		return;
+	}
+
+	// A sensible default pattern
+	e.respondWith(
+		caches.match(e.request).then(function (response) {
+			return response || fetch(e.request);
+		})
+	);
+	// var requestURL = new URL(e.request.url);
+	/*
+		if(requestURL.hostname != 'api.thegrid.com') {
+			// console.log('Request is to grid api');
+		} else if(/\.googleapis\.com$/.test(requestURL.hostname) ||
+							/\.gstatic\.com$/.test(requestURL.hostname)) {
+			// console.log('Request is to google api');
+		} else {
+			console.log('[ServiceWorker] Fetch', e.request.url);
+	
+			// e.respondWidth Responds to the fetch event
+			e.respondWith(
+	
+				// Check in cache for the request being made
+				caches.match(e.request)
+	
+	
+					.then(function(response) {
+	
+						// If the request is in the cache
+						if ( response ) {
+							console.log("[ServiceWorker] Found in Cache", e.request.url, response);
+							// Return the cached version
+							return response;
+						}
+	
+						// If the request is NOT in the cache, fetch and cache
+	
+						var requestClone = e.request.clone();
+						fetch(requestClone)
+							.then(function(response) {
+	
+								if ( !response ) {
+									console.log("[ServiceWorker] No response from fetch ")
+									return response;
+								}
+	
+								var responseClone = response.clone();
+	
+								//  Open the cache
+								return caches.open(cacheName).then(function(cache) {
+	
+									// Put the fetched response in the cache
+									cache.put(e.request, responseClone);
+									// console.log('aaaaaaaaa', e.request, responseClone);
+									console.log('[ServiceWorker] New Data Cached', e.request.url);
+	
+									console.log('response', response);
+									// Return the response
+									return response;
+	
+								}); // end caches.open
+	
+							})
+							.catch(function(err) {
+								console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
+							});
+	
+	
+					}) // end caches.match(e.request)
+			); // end e.respondWith
+	
+		}
+	*/
 });
