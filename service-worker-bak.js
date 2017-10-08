fdec73f37aff46e0781f871d9cd5ae02cf09f77f
Leomar Bucud <leomarbucud@gmail.com>
2017-10-08 13:57:12 +0400

    offset duplicate marker positon by three meters,

--- service-worker-bak.js
+++ service-worker-bak.js
@@ -14,7 +14,7 @@
 //   'Service worker disabled for development, will be generated at build time.'
 // );
 // Set a name for the current cache
-var cacheName = 'v1.3.6';
+var cacheName = 'v1.3.7';
 
 // Default files to always cache
 var cacheFiles = [
