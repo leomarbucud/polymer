73ecf9b0a87ee22c7ddf4188763f6b23ddddde06
Leomar Bucud <leomarbucud@gmail.com>
2017-11-13 12:05:35 +0400

    add related post to search, redesign user profile: version 1.2.1

--- service-worker-bak.js
+++ service-worker-bak.js
@@ -14,7 +14,7 @@
 //   'Service worker disabled for development, will be generated at build time.'
 // );
 // Set a name for the current cache
-var cacheName = '1.2.0';
+var cacheName = '1.2.1';
 
 // Default files to always cache
 var cacheFiles = [
